38a434071161de85c39b96af296066a827abf686
imen-c <imen.cheref@outlook.fr>
2023-09-04 02:14:08 +0200

    init

---
+++ admin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  admin
+//
+//  Created by imen cheref on 04/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

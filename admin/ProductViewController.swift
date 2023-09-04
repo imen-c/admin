5d57382c0eeef01c8dfe7d12e53cddd368850aa5
imen-c <imen.cheref@outlook.fr>
2023-09-05 00:28:09 +0200

    ProductController

---
+++ admin/ProductViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ProductViewController.swift
+//  admin
+//
+//  Created by imen cheref on 04/09/2023.
+//
+
+import UIKit
+
+class ProductViewController: UIViewController {
+
+    @IBOutlet weak var returnButton: UIButton!
+    @IBOutlet weak var bigTitle: UILabel!
+    @IBOutlet weak var addProductButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- admin/ViewController.swift
+++ admin/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bodyMenu: UIView!
+    @IBOutlet weak var stackButtonMenu: UIStackView!
+    
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    
+    @IBOutlet weak var leftSquareButton: UIButton!
+    @IBOutlet weak var rightSquareButton: UIButton!
+    
+    @IBOutlet weak var BigButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.

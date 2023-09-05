5564e000a47ec9467121e706daaa8fda39d4b114
imen-c <imen.cheref@outlook.fr>
2023-09-06 01:15:52 +0200

    formulaire addProduct + install alamofire

--- admin/AddProductViewController.swift
+++ admin/AddProductViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class AddProductViewController: UIViewController {
 
+    @IBOutlet weak var returnButton: UIButton!
+    @IBOutlet weak var bigTitle: UILabel!
+    @IBOutlet weak var nameTitle: UILabel!
+    @IBOutlet weak var editName: UITextField!
+    @IBOutlet weak var descriptionTitle: UILabel!
+    @IBOutlet weak var editDescription: UITextView!
+    @IBOutlet weak var priceTitle: UILabel!
+    @IBOutlet weak var editPrice: UITextField!
+    @IBOutlet weak var quantityTitle: UILabel!
+    @IBOutlet weak var editQuantity: UITextField!
+    
+    @IBOutlet weak var sizeSTitle: UILabel!
+    @IBOutlet weak var editS: UITextField!
+    @IBOutlet weak var sizeMTitle: UILabel!
+    @IBOutlet weak var editM: UITextField!
+    @IBOutlet weak var sizeLTitle: UILabel!
+    @IBOutlet weak var editL: UITextField!
+    @IBOutlet weak var sizeXLTitle: UILabel!
+    @IBOutlet weak var editXL: UITextField!
+    @IBOutlet weak var sizeXXLTitle: UILabel!
+    @IBOutlet weak var editXXL: UITextField!
+    
+    @IBOutlet weak var categoryTitle: UILabel!
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    @IBOutlet weak var addCategoryButton: UIButton!
+    @IBOutlet weak var imageInputView: UIView!
+    @IBOutlet weak var confirmButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
